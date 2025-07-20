fa814bc14379fa0341c89491e0bc95933da19f7b
syihabb74 <syihabb74@gmail.com>
2025-07-20 21:06:09 +0700

    selectionSort

---
+++ selection-sort/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort (arr) {
+    for ( let i = 0 ; i < arr.length ; i++ ) {
+        let minIndex = i;
+        let j = i
+        while (j < arr.length ) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j
+            }
+            j++
+        }
+        console.log(arr[minIndex]);
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr
+}
+
+
+console.log(selectionSort([1,7,39,10,2,-1]));
